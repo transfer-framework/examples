551cbfc414bd8929db84677cc1286c9a073cf52a
Harald Tollefsen <harald@netmaking.no>
2015-11-07 00:28:12 +0100

    Retructured to handle more examples in the future

--- src/Transfer/Examples/YamlToEzPlatformContentType/Manifest/YamlToEzPlatformContentTypeManifest.php
+++ src/Transfer/Examples/YamlToEzPlatformContentType/Manifest/YamlToEzPlatformContentTypeManifest.php
@@ -1,23 +1,40 @@
 <?php
 
-namespace Transfer\Examples\Manifest;
+namespace Transfer\Examples\YamlToEzPlatformContentType\Manifest;
 
+use eZ\Publish\API\Repository\Repository;
 use Transfer\Commons\Stream\Adapter\StreamAdapter;
 use Transfer\Commons\Yaml\Worker\Transformer\YamlToArrayTransformer;
 use Transfer\EzPlatform\Adapter\EzPlatformAdapter;
 use Transfer\Manifest\AbstractManifest;
 use Transfer\Manifest\ManifestInterface;
 use Transfer\Procedure\ProcedureBuilder;
+use Transfer\Processor\EventDrivenProcessor;
 use Transfer\Processor\EventSubscriber\Logger;
 use Transfer\Processor\ProcessorInterface;
+use Transfer\Processor\SequentialProcessor;
 
-class YamlToEzPlatformContentTypeManifest extends AbstractManifest
+class YamlToEzPlatformContentTypeManifest implements ManifestInterface
 {
 
     /**
-     * Returns manifest name.
-     *
-     * @return string Manifest name
+     * @var Repository
+     */
+    protected $repository;
+
+    /**
+     * @var SequentialProcessor
+     */
+    protected $processor;
+
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+        $this->processor = new SequentialProcessor();
+    }
+
+    /**
+     * @inheritdoc
      */
     public function getName()
     {
@@ -25,39 +42,43 @@ class YamlToEzPlatformContentTypeManifest extends AbstractManifest
     }
 
     /**
-     * Configures procedure builder.
-     *
-     * @param ProcedureBuilder $builder Procedure builder
+     * @inheritdoc
+     */
+    public function getProcessor()
+    {
+        return $this->processor;
+    }
+
+    /**
+     * @inheritdoc
      */
     public function configureProcedureBuilder(ProcedureBuilder $builder)
     {
         $builder
             ->createProcedure('import')
                 ->createProcedure('contenttype')
-                    ->addSource(new StreamAdapter(fopen(__DIR__.'/resources/yaml/frontpage.yml')))
+                    ->addSource(new StreamAdapter(fopen(__DIR__.'/../resources/yaml/frontpage.yml', 'r')))
                         ->addWorker(new YamlToArrayTransformer())
-                    ->addTarget(new EzPlatformAdapter())
+                    ->addTarget(new EzPlatformAdapter(array('repository' => $this->repository)))
                 ->end()
             ->end()
         ;
     }
 
     /**
-     * Configures processor.
-     *
-     * @param ProcessorInterface $processor Processor
+     * @inheritdoc
      */
     public function configureProcessor(ProcessorInterface $processor)
     {
+        /*
         $logger = new Logger('default');
         $logger->pushHandler(new StreamHandler(sprintf('%s/%s.log', 'var/log/transfer/contenttype', date('Y-m-d')), Logger::DEBUG));
-
         if ($processor instanceof EventDrivenProcessor) {
             $processor->setLogger($logger);
             $processor->setStorageStack($this->stack);
-
-            \Mage::register('isSecureArea', true);
             $processor->addListener(TransferEvents::POST_PROCESS, array(new ModelRemover($this->tracker), 'deactivate'));
         }
+        */
     }
+
 }
--- src/Transfer/Examples/YamlToEzPlatformContentType/Manifest/chain.php
+++ src/Transfer/Examples/YamlToEzPlatformContentType/Manifest/chain.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Transfer\Examples\Manifest;
+namespace Transfer\Examples\YamlToEzPlatformContentType\Manifest;
 
 use Transfer\Manifest\ManifestChain;
 
