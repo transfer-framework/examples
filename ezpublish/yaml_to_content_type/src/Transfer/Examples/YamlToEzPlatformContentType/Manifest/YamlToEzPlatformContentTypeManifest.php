581ec90fb62800907d86e74bc2ba4b15e8b6d0ef
Harald Tollefsen <harald@netmaking.no>
2015-12-01 23:08:27 +0100

    Implements yaml to content type manifest

---
+++ ezpublish/yaml_to_content_type/src/Transfer/Examples/YamlToEzPlatformContentType/Manifest/YamlToEzPlatformContentTypeManifest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Transfer\Examples\YamlToEzPlatformContentType\Manifest;
+
+use eZ\Publish\API\Repository\Repository;
+use Transfer\Commons\Stream\Adapter\DirectoryStreamAdapter;
+use Transfer\Commons\Yaml\Worker\Transformer\YamlToArrayTransformer;
+use Transfer\EzPlatform\Adapter\EzPlatformAdapter;
+use Transfer\EzPlatform\Worker\Transformer\ArrayToEzPlatformContentTypeObjectTransformer;
+use Transfer\Manifest\ManifestInterface;
+use Transfer\Procedure\ProcedureBuilder;
+use Transfer\Processor\EventDrivenProcessor;
+use Transfer\Processor\ProcessorInterface;
+use Transfer\Processor\SequentialProcessor;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+class YamlToEzPlatformContentTypeManifest implements ManifestInterface
+{
+    /**
+     * @var Repository
+     */
+    protected $repository;
+
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    /**
+     * @var SequentialProcessor
+     */
+    protected $processor;
+
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+        $this->processor = new SequentialProcessor();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'yaml_to_ezplatform_contenttype';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getProcessor()
+    {
+        return $this->processor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureProcedureBuilder(ProcedureBuilder $builder)
+    {
+        $builder
+            ->createProcedure('import')
+                ->createProcedure('contenttype')
+                    ->addSource(new DirectoryStreamAdapter(__DIR__.'/../resources/yaml'))
+                        ->addWorker(new YamlToArrayTransformer())
+                        ->addWorker(new ArrayToEzPlatformContentTypeObjectTransformer())
+                    ->addTarget(new EzPlatformAdapter(array('repository' => $this->repository)))
+                ->end()
+            ->end()
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureProcessor(ProcessorInterface $processor)
+    {
+        $logger = new Logger('default');
+        $logger->pushHandler(new StreamHandler(sprintf('%s/%s.log', 'ezpublish/logs/transfer/contenttype', date('Y-m-d')), Logger::DEBUG));
+        if ($processor instanceof EventDrivenProcessor) {
+            $processor->setLogger($logger);
+        }
+    }
+}
